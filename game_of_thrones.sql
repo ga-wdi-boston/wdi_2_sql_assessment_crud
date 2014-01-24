b68ce3b8fb86b05cea3f0067f5d4b074ae3ddccf
Yiyang Song <songyiyang2012@gmail.com>
2014-01-24 09:26:52 -0500

    Yiyang database

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -1,1 +1,14 @@
- 
+CREATE DATABASE westeros;
+drop table houses;
+drop table people;
+Create table houses (name text, symbol text, saying text);
+create table people (name text, favorite_weapon text, age INTEGER);
+
+insert into houses (name, symbol, saying) VALUES ('a', 'arrow', 'a');
+insert into houses (name, symbol, saying) VALUES ('B', 'brow', 'b');
+insert into houses (name, symbol, saying) VALUES ('C', 'crr', 'c');
+insert into people (name, favorite_weapon, age) VALUES('aa', 'aab', 30);
+insert into people (name, favorite_weapon, age) VALUES('bb', 'bbc', 40);
+insert into people (name, favorite_weapon, age) VALUES('cc', 'ccd', 32);
+
+drop database westeros;
