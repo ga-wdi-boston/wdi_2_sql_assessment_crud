7771d1b00c048de64a89bad1a24f73cd54f4da47
emreiser <emilie.reiser@gmail.com>
2014-01-24 09:26:30 -0500

    GOT quiz

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -1,1 +1,13 @@
- 
+CREATE DATABASE westeros;
+CREATE TABLE houses (name text, symbol text, saying text);
+CREATE TABLE people (name text, favorite_weapon text, age integer);
+
+INSERT INTO houses (name, symbol, saying) VALUES ('Ni', 'Shrub', 'We are the knights');
+INSERT INTO houses (name, symbol, saying) VALUES ('Dawn', 'Shield', 'Darkness retreats');
+INSERT INTO houses (name, symbol, saying) VALUES ('Lepers', 'Stick', 'Keep your distance');
+
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Ellen', 'wrench', 30);
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Paris', 'candlestick', 42);
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Chase', 'teeth', 19);
+
+DROP DATABASE westeros;
