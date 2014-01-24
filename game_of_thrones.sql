f82812e3737405e3b45739cdc4b7aa120e11f197
K Mak <karen.mak@stx.oxon.org>
2014-01-24 09:25:33 -0500

    quiz

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -1,1 +1,18 @@
- 
+CREATE DATABASE westeros;
+
+\c westeros;
+
+CREATE TABLE houses(name text, symbol text, saying text);
+
+CREATE TABLE people (name text, favorite_weapon text, age integer);
+
+INSERT INTO houses (name, symbol, saying) VALUES ('Apple', 'apple', 'there is no escaping us');
+INSERT INTO houses (name, symbol, saying) VALUES ('Orange', 'orange', 'orange juice for the win');
+INSERT INTO houses (name, symbol, saying) VALUES ('Pear', 'pear', 'you will regret it');
+
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Leah', 'Broadsword', 19);
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Melvin', 'Dagger', 49);
+INSERT INTO people (name, favorite_weapon, age) VALUES ('Keeve', 'Bare Hands', 9);
+
+DROP DATABASE westeros;
+
