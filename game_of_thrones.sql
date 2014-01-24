13869f80dac7f999cd77f8f79fbe7bc4db6c4ba3
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-24 09:24:35 -0500

    database quiz

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -1,1 +1,20 @@
- 
+praggya=# CREATE DATABASE westeros;
+CREATE DATABASE
+praggya=# CREATE TABLE houses (name TEXT, symbol TEXT, saying TEXT);
+CREATE TABLE
+praggya=# CREATE TABLE people (name TEXT, favorite_weapon TEXT, age INTEGER);
+CREATE TABLE
+praggya=# INSERT INTO houses VALUES ('Toms', 'Cat', 'Never say never');
+INSERT 0 1
+praggya=# INSERT INTO houses VALUES ('Brown', 'Cow', 'Moooo');
+INSERT 0 1
+praggya=# INSERT INTO houses VALUES ('Polaszewski', 'Ram', 'Giving up is for losers');
+INSERT 0 1
+praggya=# INSERT INTO people VALUES ('Brad', 'Mace', 23);
+INSERT 0 1
+praggya=# INSERT INTO people VALUES ('Kevin', 'Spear', 34);
+INSERT 0 1
+praggya=# INSERT INTO people VALUES ('Marcus', 'Club', 45);
+INSERT 0 1
+praggya=# DROP DATABASE westeros;
+DROP DATABASE
