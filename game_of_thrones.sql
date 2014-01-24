584242338852b3464f0a43bd74c13c29923fa0cf
Matt Higgins <higginsmt@gmail.com>
2014-01-24 09:28:35 -0500

    added semicolons

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -7,10 +7,10 @@ CREATE TABLE houses (name text, symbol text, saying text);
 
 CREATE TABLE people (name text, favorite_weapon text, age integer);
 
-INSERT INTO houses (name, symbol, saying) VALUES ("House of Zeus", "Upside-down Skull", "THUNDERRR!!!!")
-INSERT INTO houses (name, symbol, saying) VALUES ("House of Mercury", "Happpy Face", "Smile!!!!")
-INSERT INTO houses (name, symbol, saying) VALUES ("House of Athena", "Runner", "RUN!!!!")
+INSERT INTO houses (name, symbol, saying) VALUES ("House of Zeus", "Upside-down Skull", "THUNDERRR!!!!");
+INSERT INTO houses (name, symbol, saying) VALUES ("House of Mercury", "Happpy Face", "Smile!!!!");
+INSERT INTO houses (name, symbol, saying) VALUES ("House of Athena", "Runner", "RUN!!!!");
 
-INSERT INTO people (name, favorite_weapon, age) VALUES ("Joe", "Sword", 40)
-INSERT INTO people (name, favorite_weapon, age) VALUES ("Mike", "Mace", 20)
-INSERT INTO people (name, favorite_weapon, age) VALUES ("Todd", "Nunchuks", 34)
+INSERT INTO people (name, favorite_weapon, age) VALUES ("Joe", "Sword", 40);
+INSERT INTO people (name, favorite_weapon, age) VALUES ("Mike", "Mace", 20);
+INSERT INTO people (name, favorite_weapon, age) VALUES ("Todd", "Nunchuks", 34);
