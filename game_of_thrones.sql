74340a7b27f3af45781e712f55a513696d37b4e0
Emma Gardner <emmagardner@outlook.com>
2014-06-19 10:31:37 -0400

    finished assessment- CRUD

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -5,15 +5,39 @@ CREATE DATABASE westeros;
 -- 1. Create two tables:
 --   `houses` has an id (primary key), name (text), a sigil (text) and a motto (text)
 --   `people` has an id (primary key), name (text), weapon (text), and age (integer)
+CREATE TABLE houses (
+id SERIAL PRIMARY KEY,
+name TEXT,
+sigil TEXT,
+motto TEXT
+);
 
+CREATE TABLE people (
+id SERIAL PRIMARY KEY,
+name TEXT,
+weapon TEXT,
+age INTEGER
+);
 
 -- 2. Create at least three houses and three people with varying attributes.
+INSERT INTO houses (name, sigil, motto)
+VALUES
+('Lanister', 'Lion', 'We are the lions!'),
+('Stark', 'Wolf', 'We are the lions!'),
+('', 'Dragon', 'We are the dragons!');
 
+INSERT INTO people (name, weapon, age)
+VALUES
+('John Snow', 'axe', 28),
+('Geoffrey', 'sword', 16),
+('Someone Stark', 'sword', 30);
 
 -- 3. Write a query to get all attributes of people greater than a certain age *or* having a specific weapon.
-
+SELECT * FROM people WHERE age > 18 OR WHERE weapon = 'sword';
 
 -- 4. Change the motto of one of your houses. Look it up by its sigil, since you don't know the ID.
+UPDATE houses SET motto = 'Fly away dragon!' WHERE sigil = 'dragon';
 
 
 -- 5. Kill off, er, I mean delete one of your people. Look them up by their name, since you don't know the ID.
+DELETE FROM people WHERE name = 'Geoffrey';
