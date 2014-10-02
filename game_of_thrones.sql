d03a8c04d732bd7679e520cfe0edd6605bb98e67
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-10-02 09:33:24 -0400

    the quiz should be accurate to the show... and now its spoiled

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -6,14 +6,44 @@ CREATE DATABASE westeros;
 --   `houses` has an id (primary key), name (text), a sigil (text) and a motto (text)
 --   `people` has an id (primary key), name (text), weapon (text), and age (integer)
 
+CREATE TABLE house(
+  id SERIAL PRIMARY KEY, name TEXT, sigil TEXT, motto TEXT
+  );
+
+CREATE TABLE people(
+  id SERIAL PRIMARY KEY, name TEXT, weapon TEXT, age INTEGER
+  );
 
 -- 2. Create at least three houses and three people with varying attributes.
 
+INSERT INTO people (name, weapon, age)
+  VALUES('Tyrion Lannister','Crossbow', 36);
+INSERT INTO people (name, weapon, age)
+  VALUES('Joffrey Baratheon','Sword', 19);
+INSERT INTO people (name, weapon, age)
+  VALUES('Robert Baratheon','Sword', 43);
+
+INSERT INTO house (name, sigil, motto)
+  VALUES('House Baratheon','A crowned black stag rampant on a gold field', 'Ours is the Fury');
+INSERT INTO house (name, sigil, motto)
+  VALUES('House Stark','A grey direwolf on a white field.', 'Winter is Coming');
+INSERT INTO house (name, sigil, motto)
+  VALUES('House Lannister','A golden lion rampant on a crimson field.', 'Hear Me Roar!');
 
 -- 3. Write a query to get all attributes of people greater than a certain age *or* having a specific weapon.
 
+SELECT * FROM people WHERE age > 20;
 
 -- 4. Change the motto of one of your houses. Look it up by its sigil, since you don't know the ID.
 
 
+UPDATE house SET motto = 'A Lannister Always Pays His Debts' WHERE id = 2;
+
 -- 5. Kill off, er, I mean delete one of your people. Look them up by their name, since you don't know the ID.
+
+DELETE FROM people WHERE name = 'Joffrey Baratheon';
+SELECT * FROM people;
+SELECT * FROM house;
+
+
+-- I DONT WATCH GAME OF THRONES... to the internet for spoilers... and the internet dosent help that much...
