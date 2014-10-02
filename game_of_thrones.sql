81d4cef41c13810a8250dc7a62be348632dc51c5
areddy14 <areddy1441@gmail.com>
2014-10-02 09:44:55 -0400

    5/5, There isn't anything that I need to change. The only thing I may want to watch out for here is making the code itself easier to read.

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -5,9 +5,17 @@ CREATE DATABASE westeros;
 -- 1. Create two tables:
 --   `houses` has an id (primary key), name (text), a sigil (text) and a motto (text)
 --   `people` has an id (primary key), name (text), weapon (text), and age (integer)
-CREATE TABLE houses (id SERIAL PRIMARY KEY, name TEXT, sigil TEXT, motto TEXT);
-CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, weapon TEXT, age INTEGER);
-
+CREATE TABLE houses (
+	id SERIAL PRIMARY KEY, 
+	name TEXT, 
+	sigil TEXT, 
+	motto TEXT);
+CREATE TABLE people (
+	id SERIAL PRIMARY KEY, 
+	name TEXT, 
+	weapon TEXT, 
+	age INTEGER);
+--I just put everything on different lines
 -- 2. Create at least three houses and three people with varying attributes.
 INSERT INTO houses (name, sigil, motto) VALUES ('Rose', 'WereWolf', 'Time stops for no man unless he casts Stop.'), ('Awesome', 'Lion', 'Always carry brocolli everywhere you go'), ( 'Greybeard','Dragon', 'Im afraid of the toothfairy');
 INSERT INTO people (name, weapon, age) VALUES ('Avi', 'rapier', 20), ('Jack', 'battle axe', 23), ('Mark', 'lance', 25);
