6ffa5100055ea4ad2e9d94b6f7081823d07ed46a
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-02 09:21:52 -0400

    turning in quiz

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -6,14 +6,122 @@ CREATE DATABASE westeros;
 --   `houses` has an id (primary key), name (text), a sigil (text) and a motto (text)
 --   `people` has an id (primary key), name (text), weapon (text), and age (integer)
 
+CREATE TABLE houses (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  sigil TEXT,
+  motto TEXT
+);
+
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  weapon TEXT,
+  age INTEGER
+);
+
+\dt houses
+\dt people
 
 -- 2. Create at least three houses and three people with varying attributes.
 
+INSERT INTO houses (
+  name,
+  sigil,
+  motto
+)
+VALUES (
+  'Full House',
+  'Golden retriever',
+  'How rude!'
+);
+
+INSERT INTO houses (
+  name,
+  sigil,
+  motto
+)
+VALUES (
+  'Shiganshina',
+  'Titan',
+  'SEID IHR DAS ESSEN NEIN WIR SIND DER JÄGER'
+);
+
+INSERT INTO houses (
+  name,
+  sigil,
+  motto
+)
+VALUES (
+  'Bob House',
+  'Bob',
+  'I dont watch this show'
+);
+
+INSERT INTO people (
+  name,
+  weapon,
+  age
+)
+VALUES (
+  'Swordfish',
+  'A sword, geddit',
+  1
+);
+
+INSERT INTO people (
+  name,
+  weapon,
+  age
+)
+VALUES (
+  'Abe Vigoda',
+  'Abe Vigoda',
+  99
+);
+
+INSERT INTO people (
+  name,
+  weapon,
+  age
+)
+VALUES (
+  'me',
+  'ur mom',
+  24
+);
+
+SELECT * FROM houses;
+SELECT * FROM people;
 
 -- 3. Write a query to get all attributes of people greater than a certain age *or* having a specific weapon.
 
+SELECT name, weapon, age
+FROM people
+WHERE age > 50 OR weapon = 'ur mom';
 
 -- 4. Change the motto of one of your houses. Look it up by its sigil, since you don't know the ID.
 
+-- looked up UPDATE syntax here: http://www.sql.su/
+
+UPDATE houses
+SET motto = 'new motto'
+WHERE sigil = 'Bob';
+
+SELECT * FROM houses;
 
 -- 5. Kill off, er, I mean delete one of your people. Look them up by their name, since you don't know the ID.
+
+-- looked up DELETE FROM syntax here: http://www.sql.su/
+
+DELETE FROM people
+WHERE name = 'Abe Vigoda';
+
+SELECT * FROM people;
+
+-- oh no time to update http://www.abevigoda.com/ !!!!!!!!!!! rip abegoda
+
+
+
+
+-- im sick today.
