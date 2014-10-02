25775f13ffc9d72cc87a7d716e4153b3e989dd4b
shaai <shaai.sattar@gmail.com>
2014-10-02 09:39:14 -0400

    forgot to answer question 3. Resubmitting with question 3 added.

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -6,10 +6,18 @@ CREATE DATABASE westeros;
 
 -- 1. Create two tables:
 --   `houses` has an id (primary key), name (text), a sigil (text) and a motto (text)
-CREATE TABLE houses (id SERIAL PRIMARY KEY, name TEXT, sigil TEXT, motto TEXT);
+CREATE TABLE houses (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  sigil TEXT,
+  motto TEXT);
 
 --   `people` has an id (primary key), name (text), weapon (text), and age (integer)
-CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, weapon TEXT, age INTEGER);
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  weapon TEXT,
+  age INTEGER);
 
 -- 2. Create at least three houses and three people with varying attributes.
 INSERT INTO housese (name, sigil, motto) VALUES ('Sonia', 'rock', 'Power for all!');
@@ -22,7 +30,7 @@ INSERT INTO people (name, weapon, age) VALUES ('Erik', 'paper planes', 25);
 
 
 -- 3. Write a query to get all attributes of people greater than a certain age *or* having a specific weapon.
-
+SELECT * FROM people WHERE age > 25 OR weapon = 'pipe';
 
 -- 4. Change the motto of one of your houses. Look it up by its sigil, since you don't know the ID.
 UPDATE houses SET motto = 'Flower Power!' WHERE sigil = 'Sam';
