7282c7ad55bd921c5c97d31b798add0110371ca0
cyurtbil <cenky92@gmail.com>
2014-10-02 09:44:11 -0400

    Score 5/5

--- game_of_thrones.sql
+++ game_of_thrones.sql
@@ -19,6 +19,8 @@ CREATE TABLE people (
 	age INTEGER
 );
 
+-- Correct
+
 
 -- 2. Create at least three houses and three people with varying attributes.
 INSERT INTO houses (name, sigil, motto) VALUES ('elendil', 'elf', 'none');
@@ -29,19 +31,29 @@ INSERT INTO people (name, weapon, age) VALUES ('legolas', 'bow', '300');
 INSERT INTO people (name, weapon, age) VALUES ('aragorn', 'sword', '35');
 INSERT INTO people (name, weapon, age) VALUES ('saruman', 'staff', '600');
 
+-- Correct
+
 -- 3. Write a query to get all attributes of people greater than a certain age *or* having a specific weapon.
 SELECT *
 FROM people
 WHERE age > 100 OR weapon = 'bow';
+
+-- Correct
 
 -- 4. Change the motto of one of your houses. Look it up by its sigil, since you don't know the ID.
 UPDATE houses
 SET name = 'gondor'
 WHERE sigil = 'urukai';
 
+-- Correct
+
 -- 5. Kill off, er, I mean delete one of your people. Look them up by their name, since you don't know the ID.
 DELETE FROM people
 WHERE name = 'saruman';
+
+-- Correct
+
+-- Score 5/5
 
 -- CENK YURTBILIR
 
